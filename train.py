624ff3e8691e5ec19ce8b35604b0393594439666
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2025-06-17 16:40:04 +0700

    update train function

--- train.py
+++ train.py
@@ -2,18 +2,15 @@ import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
 from utils import (
+    build_hyperparameters,
     build_student_model,
     compute_loss,
-    negative_sampling_loss,
     compute_metrics,
-    colorize_segmentation,
+    normalize_entropy,
+    colorize_segmentation
 )
 from dataset.ugm import UGMDataModule
-from transformers import get_cosine_schedule_with_warmup
-import os
-import itertools
-from models.refinement import EncoderPRN
-from math import cos, pi
+from rl_threshold_callback import RLThresholdCallback
 
 
 class BaseModel(pl.LightningModule):
@@ -37,78 +34,61 @@ class BaseModel(pl.LightningModule):
         self.weight_decay = weight_decay
         self.num_classes = 19
         self.ignore_index = 255
-        self.confidence_threshold = kwargs.get("confidence_threshold", 0)
+        self.T = kwargs.get("T", 1.0)
+        self.entropy_threshold = kwargs.get("entropy_threshold", 1.0)
         self.use_refinement = kwargs.get("use_refinement", False)
         self.use_curriculum_thr = kwargs.get("use_curriculum_thr", False)
         self.use_neg_loss = kwargs.get("use_neg_loss", False)
+        
+        if self.entropy_threshold == "adaptive":
+            self.adaptive_entropy_thresholds = 0.0
 
         # Models
         self.student = build_student_model(
             encoder=encoder, decoder=decoder, weight=weight, dropout=decoder_dropout
         )
-        
-        self.prn = EncoderPRN(
-            in_channels=self.num_classes+3,
-            num_classes=self.num_classes,
-            mid_channels=32,
-        )
 
+    def forward(self, x):
+        return self.student(x)
+    
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(
-            list(self.student.parameters()) + list(self.prn.parameters()), lr=self.lr, weight_decay=self.weight_decay
+        optimizer = torch.optim.AdamW(self.student.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, mode="min", factor=0.2, patience=2, verbose=True
         )
-
-        total_steps = self.trainer.estimated_stepping_batches
-        warmup_steps = int(0.01 * total_steps)  # 1% warmup
-        scheduler = get_cosine_schedule_with_warmup(
-            optimizer, num_warmup_steps=warmup_steps, num_training_steps=total_steps
-        )
-        scheduler_config = {
-            "scheduler": scheduler,
-            "interval": "step",  # call every training step
-            "frequency": 1,
-        }
-
-        return [optimizer], [scheduler_config]
-
+        return [optimizer], [{"scheduler": scheduler, "interval": "epoch", "monitor": "loss/total/val"}]
+    
     def step(self, batch, stage="train"):
-        images, t_logits = batch
-        s_logits = self.student(images)
+        if self.use_refinement:
+            images, t_logits, pseudo_labels = batch
+        else:
+            images, t_logits = batch
+            t_probs = F.softmax(t_logits, dim=1)
+            _, pseudo_labels = t_probs.max(dim=1)
+        
+        if stage == "train":
+            t_probs = F.softmax(t_logits, dim=1)
+            entropy = normalize_entropy(t_probs)
+            if self.entropy_threshold == "adaptive":
+                thresholds = self.adaptive_entropy_thresholds
+                mask = entropy < thresholds
+                pseudo_labels[~mask] = self.ignore_index
+            elif isinstance(self.entropy_threshold, (int, float)) and self.entropy_threshold < 1.0:
+                thresholds = self.entropy_threshold
+                mask = entropy < thresholds
+                pseudo_labels[~mask] = self.ignore_index
+            
+        s_logits = self(images)
         preds = torch.argmax(s_logits, dim=1)
-
-        t_probs = F.softmax(t_logits, dim=1)
-        conf, pseudo_labels = torch.max(t_probs, dim=1)
-
-        # Apply confidence threshold to pseudo labels
-        if ((self.confidence_threshold > 0) or self.use_curriculum_thr) and stage == "train":
-            if self.use_curriculum_thr:
-                self.confidence_threshold = self.curriculum_threshold(self.current_epoch, self.trainer.max_epochs)
-            conf_mask = conf > self.confidence_threshold
-            pseudo_labels[~conf_mask] = self.ignore_index
-          
+        
         loss = compute_loss(
             s_logits,
             pseudo_labels,
             t_logits if self.use_kd else None,
             use_kd=self.use_kd,
             kd_weight=self.kd_weight,
+            T=self.T,
         )
-        
-        # Apply negative sampling loss if enabled
-        if self.use_neg_loss and stage == "train":
-            neg_loss = negative_sampling_loss(s_logits, t_probs, confidence_threshold=self.confidence_threshold)
-            loss["total"] += neg_loss
-            loss["neg"] = neg_loss  
-        
-        # Apply Refinement to teacher logits if enabled
-        if self.use_refinement and stage == "train":
-            refined_logits = self.prn(images, t_logits)
-            loss_prn = F.cross_entropy(refined_logits, pseudo_labels, ignore_index=self.ignore_index)
-            pseudo_labels = refined_logits.argmax(dim=1)
-
-            loss["total"] += loss_prn
-            loss["prn"] = loss_prn
-        
             
         metrics = compute_metrics(
             preds,
@@ -125,7 +105,7 @@ class BaseModel(pl.LightningModule):
             "pred": preds[0],
         }
 
-    def logging(self, step_output, stage="train"):
+    def log_step_outputs(self, step_output, stage="train"):
         # Log losses
         self.log(
             f"loss/total/{stage}",
@@ -156,10 +136,10 @@ class BaseModel(pl.LightningModule):
                 )
 
         # Log images every 10 steps for non-training stages
-        if stage != "train":
+        if stage != "train" and hasattr(self.logger, "experiment") and hasattr(self.logger.experiment, "add_image"):
             self.logger.experiment.add_image(
                 f"{stage}_samples/image",
-                step_output["image"],
+                step_output["image"].detach().cpu(),
                 self.global_step,
                 dataformats="CHW",
             )
@@ -177,82 +157,67 @@ class BaseModel(pl.LightningModule):
             )
 
     def on_train_epoch_start(self):
-        self.log("lr", self.trainer.optimizers[0].param_groups[0]["lr"])
+        if hasattr(self, "trainer") and hasattr(self.trainer, "optimizers"):
+            if self.trainer.optimizers:
+                lr = self.trainer.optimizers[0].param_groups[0]["lr"]
+                self.log("lr", lr, prog_bar=True, sync_dist=True)
 
     def training_step(self, batch, _):
         output = self.step(batch, stage="train")
-        self.logging(output, stage="train")
+        self.log_step_outputs(output, stage="train")
         return output["loss"]["total"]
 
     def validation_step(self, batch, _):
         output = self.step(batch, stage="val")
-        self.logging(output, stage="val")
+        self.log_step_outputs(output, stage="val")
         return output["loss"]["total"]
 
     def test_step(self, batch, _):
         output = self.step(batch, stage="test")
-        self.logging(output, stage="test")
+        self.log_step_outputs(output, stage="test")
         return output["loss"]["total"]
-
-    def curriculum_threshold(self, epoch, total_epochs, min_thr=0.75, max_thr=0.95):
-        threshold = min_thr + 0.5 * (max_thr - min_thr) * (1 + cos(pi * epoch / total_epochs))
-        self.log("curriculum_threshold", threshold, on_step=True, on_epoch=True)
-        return threshold
-
+    
+    def predict_step(self, batch, _):
+        self.eval()
+        with torch.no_grad():
+            output = self.step(batch, stage="predict")
+        return output["pred"]
+    
+    
 if __name__ == "__main__":
     torch.hub.set_dir("/media/esr/ssd0/cache")
     pl.seed_everything(42, workers=True)
 
     # Parameters
-    USE_KD = [True]
+    USE_KD = [False]
+    T = [2.0]
     KD_WEIGHT = [0.001]
     LR = [1e-3]
-    BATCH_SIZE = [12]  # Batch sizes
     ENCODER = ["timm-efficientnet-b0"]
     DECODER = ["unet"]
     WEIGHT = ["imagenet"]
     DECODER_DROPOUT = [0.5]
-    CONFIDENCE_THRESHOLD = [0.95]
-    USE_REFINEMENT = [True]
-    USE_CURRICULUM_THR = [True]
-    USE_NEG_LOSS = [True]
+    ENTROPY_THRESHOLD = [1.0]
+    USE_REFINEMENT = [False]
     
     SIZE = (256, 256)
+    BATCH_SIZE = 12  # Batch sizes
 
-    all_combinations = list(
-        itertools.product(
-            USE_KD,
-            KD_WEIGHT,
-            LR,
-            BATCH_SIZE,
-            ENCODER,
-            DECODER,
-            WEIGHT,
-            DECODER_DROPOUT,
-            CONFIDENCE_THRESHOLD,
-            USE_REFINEMENT,
-            USE_CURRICULUM_THR,
-            USE_NEG_LOSS,
-        )
-    )
-
-    keys = [
-        "use_kd",
-        "kd_weight",
-        "lr",
-        "batch_size",
-        "encoder",
-        "decoder",
-        "weight",
-        "decoder_dropout",
-        "confidence_threshold",
-        "use_refinement",
-        "use_curriculum_thr",
-        "use_neg_loss",
-    ]
-    combinations_dict = [dict(zip(keys, values)) for values in all_combinations]
-
+    hyperparameter_list = {
+        "use_kd": USE_KD,
+        "T": T,
+        "kd_weight": KD_WEIGHT,
+        "lr": LR,
+        "encoder": ENCODER,
+        "decoder": DECODER,
+        "weight": WEIGHT,
+        "decoder_dropout": DECODER_DROPOUT,
+        "entropy_threshold": ENTROPY_THRESHOLD,
+        "use_refinement": USE_REFINEMENT,
+    }
+    combinations_dict = build_hyperparameters(hyperparameter_list)
     print(f"Total combinations: {len(combinations_dict)}")
+
 
     for i, params in enumerate(combinations_dict):
         model = BaseModel(
@@ -260,59 +225,59 @@ if __name__ == "__main__":
             decoder=params["decoder"],
             weight=params["weight"],
             use_kd=params["use_kd"],
+            T=params["T"],
             kd_weight=params["kd_weight"],
             lr=params["lr"],
             decoder_dropout=params["decoder_dropout"],
-            confidence_threshold=params["confidence_threshold"],
+            entropy_threshold=params["entropy_threshold"],
             use_refinement=params["use_refinement"],
-            use_curriculum_thr=params["use_curriculum_thr"],
-            use_neg_loss=params["use_neg_loss"],
         )
 
         dm = UGMDataModule(
             root="/media/esr/ssd0/dataset/2025-01-10/",
             return_teacher_logits=True,
-            batch_size=params["batch_size"],
+            use_refinement=params["use_refinement"],
+            entropy_threshold=1.0,
+            batch_size=BATCH_SIZE,
             size=SIZE,
         )
         dm.setup()
 
-        kd_str = f"_kd_alpha_{params['kd_weight']}" if params["use_kd"] else ""
-        if params['confidence_threshold'] > 0 or params['use_refinement'] or params['use_curriculum_thr']:
-            version = (
-                f"test_conf-{params['confidence_threshold']}_{params['weight']}_dropout-{params['decoder_dropout']}_lr-{params['lr']}_batch-{params['batch_size']}"
-                f"{kd_str}_{SIZE[0]}x{SIZE[1]}"
-            )
-            if params['use_refinement']:
-                version += "_refinement"
-            if params['use_curriculum_thr']:
-                version += "_curriculum_thr"
-            if params['use_neg_loss']:
-                version += "_neg_loss"
-        else:
-            version = (
-                f"base_{params['weight']}_dropout-{params['decoder_dropout']}_lr-{params['lr']}_batch-{params['batch_size']}"
-                f"{kd_str}_{SIZE[0]}x{SIZE[1]}"
-            )
+
+        kd_str = f"_kd_T{params['T']}_w-{params['kd_weight']}" if params["use_kd"] else ""
+        version = (
+            f"{params['weight']}_dropout-{params['decoder_dropout']}_lr-{params['lr']}"
+            f"{kd_str}_{SIZE[0]}x{SIZE[1]}"
+        )
+        if params['use_refinement']:
+            if params['entropy_threshold']:
+                prefix = f"refinement_thr-{params['entropy_threshold']}"
+            else:
+                prefix = "refinement"
+            version = f"{prefix}_{version}"
             
-            
-        name = f"{params['encoder']}_{params['decoder']}"
+        name = f"test_{params['encoder']}_{params['decoder']}"
         logger = pl.loggers.TensorBoardLogger("logs/", name=name, version=version)
         trainer = pl.Trainer(
-            max_epochs=100,
+            max_epochs=20,
             accelerator="gpu",
             devices=1,
+            precision=16,
             logger=logger,
             callbacks=[
-                pl.callbacks.EarlyStopping(
-                    monitor="loss/total/val", patience=10, mode="min", verbose=True
+                # pl.callbacks.EarlyStopping(
+                #     monitor="loss/total/val", patience=7, mode="min", verbose=True
+                # ),
+                pl.callbacks.ModelCheckpoint(
+                    monitor="loss/total/val",
+                    dirpath=f"saved_models/{name}/{version}",
+                    filename="best",
+                    save_top_k=1,
+                    mode="min",
                 ),
+                pl.callbacks.LearningRateMonitor(logging_interval='epoch'),
+                RLThresholdCallback(metric_name="iou/micro_imagewise/val", epsilon=0.1)
             ],
         )
         trainer.fit(model, dm)
         trainer.test(model, dm)
-
-        # Save the model
-        save_dir = f"saved_models/{name}/{version}/last.pth"
-        os.makedirs(os.path.dirname(save_dir), exist_ok=True)
-        torch.save(model.state_dict(), save_dir)
